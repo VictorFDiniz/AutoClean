ffb9735afba0422b7d8c0e6872f05c8fad3ca11e
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-06-28 14:47:13 -0300

    Update AutoClean.sh

--- AutoClean.sh
+++ AutoClean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### BEGIN INIT INFO
-# Provides:          otimiza
+# Provides:          AutoClean
 # Required-Start:    $remote_fs $syslog
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
@@ -8,10 +8,11 @@
 ### END INIT INFO
 
 while :; do
-
+#Calculates the current percentage of free RAM available
 _ram_avl=`free | grep Mem | awk '{print $4/$2 * 100.0}' | awk -F'.' '{print $1}'`
+#If free percentage is less than or equal to 20% of total RAM trigger the cleanup
 if [ $_ram_avl -le 20 ]; then
-sync; echo 3 > /proc/sys/vm/drop_caches
+sync; echo 1 > /proc/sys/vm/drop_caches
 fi
 sleep 3.5
 done &
