674f4b6abfd1d72d37c1c44f102d06b3aaa3be5d
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-06-30 00:39:44 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -23,9 +23,10 @@ if [[ -e /etc/init.d/auto-clean.sh ]]; then
 rm -rf /etc/init.d/auto-clean.sh
 update-rc.d -f auto-clean.sh remove > /dev/null 2>&1
 killall auto-clean.sh > /dev/null 2>&1
+fi
+
 wget -c -P /etc/init.d https://raw.githubusercontent.com/VictorFDiniz/CacheAutoClean/main/auto-clean.sh > /dev/null 2>&1
 chmod 775 /etc/init.d/auto-clean.sh
-fi
 
 #Run at system startup
 update-rc.d auto-clean.sh defaults > /dev/null 2>&1
