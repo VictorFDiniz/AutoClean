98a50ada537b09ac6fe9b4cbefa0d7738b250b7e
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-07-04 14:14:18 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,6 +4,27 @@ echo "Sorry, you need to run this as root"
 exit 0
 fi
 
+fun_inst() {
+
+echo ""
+echo -e "\033[1;36minstalling..."
+sleep 1.5
+echo ""
+
+wget -c -P /etc/init.d https://raw.githubusercontent.com/VictorFDiniz/CacheAutoClean/main/auto-clean.sh > /dev/null 2>&1
+
+cd /etc/init.d
+chmod 775 auto-clean.sh
+
+#Run at system startup
+update-rc.d auto-clean.sh defaults > /dev/null 2>&1
+./auto-clean.sh
+cd /root
+rm Install.sh
+echo "DONE!"
+
+}
+
 apt-get update -y > /dev/null 2>&1
 apt-get install figlet -y > /dev/null 2>&1
 
@@ -20,26 +41,14 @@ mv Install.sh /root/Install.sh > /dev/null 2>&1
 echo 60 > /proc/sys/vm/swappiness
 
 #Checking if AutoClean is already installed
-if [[ -e /etc/init.d/auto-clean.sh ]]; then
+if [[ ! -e /etc/init.d/auto-clean.sh ]]; then
+fun_inst
+elif [[ -e /etc/init.d/auto-clean.sh ]]; then
 read -p "$(echo -e "\033[1;36mAlready installed, want to re-install \033[1;31m? \033[1;33m[Y/N]:\033[1;37m ")" -e -i n response
 [[ $response = @(n|N) ]] && exit 0
-echo ""
-echo -e "\033[1;36minstalling..."
-sleep 1.5
-echo ""
+else
 rm -rf /etc/init.d/auto-clean.sh
 update-rc.d -f auto-clean.sh remove > /dev/null 2>&1
 killall auto-clean.sh > /dev/null 2>&1
-
-wget -c -P /etc/init.d https://raw.githubusercontent.com/VictorFDiniz/CacheAutoClean/main/auto-clean.sh > /dev/null 2>&1
-
-cd /etc/init.d
-chmod 775 auto-clean.sh
-
-#Run at system startup
-update-rc.d auto-clean.sh defaults > /dev/null 2>&1
-./auto-clean.sh
-cd /root
-rm Install.sh
-echo "DONE!"
+fun_inst
 fi
