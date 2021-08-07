f92aff3b5f5fabd6faa56fd117ba54afda3035a3
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-08-07 16:01:20 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -64,10 +64,10 @@ done
   echo ""
   read -p "$(echo -e "\033[1;36mDo you want to change the swappiness \033[1;31m? \033[1;33m[Y/N]:\033[1;37m ")" -e -i y response
 [[ $response = @(n|N) ]] && rm Install.sh && sleep 0.5 && exit 0
-echo ""
 echo -e "
-\033[1;33mChoose a value of 60 for Swappiness means 
-that the system can Swap once the RAM reaches 40% usage.\033[0m"
+\033[1;33mValues for the Swappiness range from 0 to 100.
+Choose a value of 60 for Swappiness means that 
+the system can Swap once the RAM reaches 40% usage.\033[0m"
 echo ""
 while read -p "$(echo -e "\033[1;36mSet a value for Swappiness \033[1;33m[0-100]: ")" _num ; do
 if [[ $_num =~ ^[0-9]+$ ]] && (( $_num >= 0 && $_num <= 100 )); then
