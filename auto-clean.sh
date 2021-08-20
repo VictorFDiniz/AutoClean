acdae599f96ad411e65c2971157169d7fcf060de
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2021-08-20 19:15:32 -0300

    Update auto-clean.sh

--- auto-clean.sh
+++ auto-clean.sh
@@ -14,7 +14,7 @@ while :; do
 #Calculates the current percentage of free RAM available
   _ram_avl=`free | grep Mem | awk '{print $4/$2 * 100.0}' | awk -F'.' '{print $1}'`
 #If free percentage is less than or equal to _ram_trig, trigger the cleanup
-if [ $_ram_avl -le $_ram_trig ]; then
+if [[ $_ram_avl -le $_ram_trig ]]; then
   sync; echo $_cache_cln > /proc/sys/vm/drop_caches
 fi
   sleep 3.5
