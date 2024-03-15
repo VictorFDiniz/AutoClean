7e43d96f14ea816f569c6bed36a1dd8f0ec2b53f
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2024-03-15 16:19:41 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,6 +4,7 @@
 YELLOW='\033[1;33m'
 RED='\033[1;31m'
 CYAN='\033[1;36m'
+LRED='\033[1;91m'
 RESET='\033[0m'
 
 # Check if the script is running as root
@@ -83,12 +84,10 @@ script_startup() {
             chkconfig --add auto-clean.sh
             chkconfig --level 3 auto-clean.sh on
             chmod 775 /etc/init.d/auto-clean.sh
-            /bin/bash /etc/init.d/auto-clean.sh
             ;;
         "debian" | "ubuntu")
             update-rc.d auto-clean.sh defaults > /dev/null 2>&1
             chmod 775 /etc/init.d/auto-clean.sh
-            /bin/bash /etc/init.d/auto-clean.sh
             ;;
     esac
 }
@@ -124,7 +123,7 @@ configure_cache_cleanup() {
 ${CYAN}1${RED}) ${YELLOW}Automate PageCache clearing
 ${CYAN}2${RED}) ${YELLOW}Automate dentries and inodes clearing
 ${CYAN}3${RED}) ${YELLOW}Automate PageCache, dentries, and inodes clearing
-${CYAN}4${RED}) ${RED}Skip${RESET}"
+${CYAN}4${RED}) ${LRED}Skip${RESET}"
     echo ""
     while true; do
         read -p "$(echo -e "${CYAN}What do you want to do ${RED}? ${YELLOW}[1/2/3/4]:${RESET} ")" x
